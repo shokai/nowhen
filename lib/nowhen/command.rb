fbf1797b85a29c4081b17b3d8998b1cdc1ab7da0
Sho Hashimoto <hashimoto@shokai.org>
2013-04-28 15:24:43 +0900

    show command help

--- lib/nowhen/command.rb
+++ lib/nowhen/command.rb
@@ -4,9 +4,27 @@ require "args_parser"
 module NoWhen
   module Command
     def self.now(argv)
-      argv.each do |what|
+      parser = ArgsParser.parse argv do
+        arg :when, 'search tags'
+        arg :version, 'show version', :alias => :v
+        arg :help, 'show help', :alias => :h
+      end
+      if parser.has_option? :help
+        STDERR.puts "nowhen - Tagger for LifeLog v#{NoWhen::VERSION}"
+        STDERR.puts "         https://github.com/shokai/nowhen"
+        STDERR.puts parser.help
+        STDERR.puts
+        STDERR.puts "e.g."
+        STDERR.puts "  % now yummy"
+        STDERR.puts "  % when yummy"
+      elsif parser.has_option? :version
+        STDERR.puts "nowhen version #{NoWhen::VERSION}"
+      elsif parser.has_param? :when
+      elsif !parser.argv.empty?
+        what = parser.argv[0]
         tag = NoWhen::Model::Tag.new(:what => what)
         tag.save
+        puts "tagged \"#{what}\" at #{tag.created_at}"
       end
     end
   end

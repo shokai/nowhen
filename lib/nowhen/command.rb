50b9866b0f0fc20f26f95e073a8cc1b8ca006479
Sho Hashimoto <hashimoto@shokai.org>
2013-04-28 17:32:38 +0900

    add when command

--- lib/nowhen/command.rb
+++ lib/nowhen/command.rb
@@ -3,13 +3,7 @@ require "args_parser"
 
 module NoWhen
   module Command
-    def self.now(argv)
-      parser = ArgsParser.parse argv do
-        arg :when, 'search tags'
-        arg :version, 'show version', :alias => :v
-        arg :help, 'show help', :alias => :h
-      end
-      if parser.has_option? :help
+    def self.show_help(parser)
         STDERR.puts "nowhen - Tagger for LifeLog v#{NoWhen::VERSION}"
         STDERR.puts "         https://github.com/shokai/nowhen"
         STDERR.puts parser.help
@@ -17,21 +11,46 @@ module NoWhen
         STDERR.puts "e.g."
         STDERR.puts "  % now yummy"
         STDERR.puts "  % when yummy"
+    end
+
+    def self.now(argv)
+      parser = ArgsParser.parse argv do
+        arg :version, 'show version', :alias => :v
+        arg :help, 'show help', :alias => :h
+      end
+      if parser.has_option? :help
+        show_help parser
       elsif parser.has_option? :version
         STDERR.puts "nowhen version #{NoWhen::VERSION}"
-      elsif parser.has_param? :when
-        tags = NoWhen::Model::Tag.find :what => parser[:when]
-        tags.each do |tag|
-          puts tag
-        end
       elsif !parser.argv.empty?
         what = parser.argv[0].strip
         tag = NoWhen::Model::Tag.new(:what => what)
         tag.save
         puts "tagged \"#{what}\" -> #{tag.created_at}"
       else
-        STDERR.puts "argument error!"
+        show_help parser
       end
     end
+
+    def self.when(argv)
+      parser = ArgsParser.parse argv do
+        arg :version, 'show version', :alias => :v
+        arg :help, 'show help', :alias => :h
+      end
+      if parser.has_option? :help
+        show_help parser
+      elsif parser.has_option? :version
+        STDERR.puts "nowhen version #{NoWhen::VERSION}"
+      elsif !parser.argv.empty?
+        what = parser.argv[0].strip
+        tags = NoWhen::Model::Tag.find :what => what
+        tags.each do |tag|
+          puts tag
+        end
+      else
+        show_help parser
+      end
+    end
+
   end
 end

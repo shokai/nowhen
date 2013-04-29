d65feeb0dbe5b4711308567367258d76352e9b39
Sho Hashimoto <hashimoto@shokai.org>
2013-04-29 17:36:41 +0900

    read ENV[NOWHEN_DB] as db path  #5

--- lib/nowhen/model.rb
+++ lib/nowhen/model.rb
@@ -6,7 +6,7 @@ require "fileutils"
 
 module NoWhen
   module Model
-    def self.init(db_path="#{ENV['HOME']}/.nowhen.sqlite")
+    def self.init(db_path)
       FileUtils.mkdir_p File.dirname(db_path) unless File.exists? File.dirname(db_path)
       DataMapper.setup(:default, {:adapter => :sqlite, :database => db_path})
       DataMapper::Logger.new($stdout, :debug)
@@ -26,4 +26,4 @@ module NoWhen
   end
 end
 
-NoWhen::Model.init
+NoWhen::Model.init(ENV['NOWHEN_DB'] || "#{ENV['HOME']}/.nowhen.sqlite")
